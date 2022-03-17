a3a7734f69f6bbc6faffc7adfdd8362ca5157346
Daniel Zanchi <dev@danielzanchi.com>
2022-03-17 17:32:27 +0100

    removed dzmonet from keys and added resetfortest

--- Sources/DZMonetization/DZMonetization.AppData.swift
+++ Sources/DZMonetization/DZMonetization.AppData.swift
@@ -13,7 +13,7 @@ public extension DZMonetization {
     class AppData {
         
         private enum Keys: String {
-            case DZMonetization_isPremium, DZMonetization_didSeeFirstPaywall
+            case isPremium, didSeeFirstPaywall
         }
         
         public static let shared = AppData()
@@ -24,26 +24,32 @@ public extension DZMonetization {
         }
         
         public func didSeeFirstSessionPaywall() {
-            keychain.set(true, forKey: Keys.DZMonetization_didSeeFirstPaywall.rawValue)
+            keychain.set(true, forKey: Keys.didSeeFirstPaywall.rawValue)
         }
         
         public func shouldShowFirstSessionPaywall() -> Bool {
             if isPremium() == true { return false }
-            if let didSee = keychain.bool(forKey: Keys.DZMonetization_didSeeFirstPaywall.rawValue) {
+            if let didSee = keychain.bool(forKey: Keys.didSeeFirstPaywall.rawValue) {
                 return !didSee
             }
             return true
         }
         
         func setPremium(_ isPremium: Bool) {
-            keychain.set(isPremium, forKey: Keys.DZMonetization_isPremium.rawValue)
+            keychain.set(isPremium, forKey: Keys.isPremium.rawValue)
         }
         
         public func isPremium() -> Bool {
-            if let isPremium = keychain.bool(forKey: Keys.DZMonetization_isPremium.rawValue) {
+            if let isPremium = keychain.bool(forKey: Keys.isPremium.rawValue) {
                 return isPremium
             }
             return false
+        }
+        
+        public func resetForTest() {
+            #if DEBUG
+            keychain.removeAllKeys()
+            #endif
         }
         
     }
