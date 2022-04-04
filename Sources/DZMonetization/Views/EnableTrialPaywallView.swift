5dfd97a034c2d63ed1ca0c274ff9be5ba1441c88
Daniel Zanchi <danny.zanchi@gmail.com>
2022-04-04 14:36:58 +0200

    new text size and color for switch

--- Sources/DZMonetization/Views/EnableTrialPaywallView.swift
+++ Sources/DZMonetization/Views/EnableTrialPaywallView.swift
@@ -35,7 +35,7 @@ public struct EnableTrialPaywallView: View {
                     VStack(alignment: .center, spacing: 12) {
                         RestoreButtonView(dismiss: dismiss, showLoadingView: $showLoadingView)
                             .padding(.top)
-                        AppImageNoTextView(size: geometry.size.height / 5)
+                        AppImageView(size: geometry.size.height / 5)
                             .padding(.top, 6)
                         Spacer()
                         GetAccessView(size: geometry.size.height / 25)
@@ -44,52 +44,18 @@ public struct EnableTrialPaywallView: View {
                         Spacer()
                         VStack(spacing: 4) {
 							Text("try7DaysForFree")
-                                .font(.system(size: 13, weight: .medium, design: DZMonetization.UI.fontDesign))
+								.font(.system(size: DZMonetization.UI.shouldUppercase ? 12 : 13, weight: .medium, design: DZMonetization.UI.fontDesign))
 								.textCase(DZMonetization.UI.shouldUppercase ? .uppercase : .none)
 								.foregroundColor(trialIsSelected ? DZMonetization.UI.textColor : .clear)
-                            Button(action: {
-                                showLoadingView = true
-                                withAnimation {
-                                    loadingOverlayOpacity = 0.9
-                                }
-                                simpleSuccess()
-                                
-                                let productId = self.trialIsSelected ? productIdWithTrial : productIdNoTrial
-                                DZAnalytics.purchaseFlowEvent(.ce_purchase_initialized, addedParameters: ["cp_product_id": productId])
-                                InAppPuchase.shared.purchaseProduct(withId: productId) { (didComplete) in
-                                    if didComplete {
-                                        if let dismiss = dismiss {
-                                            dismiss()
-                                        } else {
-                                            presentationMode.wrappedValue.dismiss()
-                                        }
-                                    } else {
-                                        showLoadingView = false
-                                    }
-                                }
-                            }, label: {
-                                VStack(spacing: 2) {
-                                    Text(trialIsSelected ? "startButton" : "Continue")
-										.font(.system(size: 24, weight: .bold, design: DZMonetization.UI.fontDesign))
-										.textCase(DZMonetization.UI.shouldUppercase ? .uppercase : .none)
-                                        .multilineTextAlignment(.center)
-                                        .fixedSize(horizontal: false, vertical: true)
-                                }
-                                .padding(.vertical, 20)
-                                .frame(width: geometry.size.width - 50)
-                                .foregroundColor(DZMonetization.UI.textButtonColor)
-                                .background(DZMonetization.UI.accentGradient)
-								.cornerRadius(DZMonetization.UI.buttonRadius)
-                                .shadow(color: .black.opacity(0.2), radius: 8, x: 0, y: 4)
-                            })
+							PurchaseButton(showLoadingView: $showLoadingView, trialIsSelected: $trialIsSelected, frameWidth: geometry.size.width, loadingOverlayOpacity: $loadingOverlayOpacity, productIdWithTrial: productIdWithTrial, productIdNoTrial: productIdNoTrial, dismiss: dismiss)
                             if trialIsSelected {
                                 Text("PriceDescription \(price)")
-                                    .font(.system(size: 14, weight: .semibold, design: DZMonetization.UI.fontDesign))
+									.font(.system(size: DZMonetization.UI.shouldUppercase ? 13 : 14, weight: .semibold, design: DZMonetization.UI.fontDesign))
 									.textCase(DZMonetization.UI.shouldUppercase ? .uppercase : .none)
                                     .foregroundColor(DZMonetization.UI.textColor)
                             } else {
                                 Text("PriceDescriptionNoTrial \(price)")
-                                    .font(.system(size: 14, weight: .semibold, design: DZMonetization.UI.fontDesign))
+									.font(.system(size: DZMonetization.UI.shouldUppercase ? 13 : 14, weight: .semibold, design: DZMonetization.UI.fontDesign))
 									.textCase(DZMonetization.UI.shouldUppercase ? .uppercase : .none)
                                     .foregroundColor(DZMonetization.UI.textColor)
                             }
@@ -138,29 +104,77 @@ public struct EnableTrialPaywallView: View {
             }
         }
     }
-    
-    func simpleSuccess() {
-        let generator = UINotificationFeedbackGenerator()
-        generator.notificationOccurred(.success)
-    }
+
 }
 
-struct AppImageNoTextView: View {
-    @State var size: CGFloat
-    
-    var body: some View {
-        ZStack {
-            Image("paywall-image")
-                .resizable()
-                .scaledToFit()
-                .frame(height: size)
-        }
-        
-    }
+struct PurchaseButton: View {
+	@Environment(\.presentationMode) var presentationMode
+	@Binding var showLoadingView: Bool
+	@Binding var trialIsSelected: Bool
+	@State var frameWidth: CGFloat = 0
+	@Binding var loadingOverlayOpacity: Double
+	var productIdWithTrial: String
+	var productIdNoTrial: String
+	var dismiss: (() -> Void)?
+
+	var body: some View {
+		Button(action: {
+			showLoadingView = true
+			withAnimation {
+				loadingOverlayOpacity = 0.9
+			}
+			simpleSuccess()
+			
+			let productId = self.trialIsSelected ? productIdWithTrial : productIdNoTrial
+			DZAnalytics.purchaseFlowEvent(.ce_purchase_initialized, addedParameters: ["cp_product_id": productId])
+			InAppPuchase.shared.purchaseProduct(withId: productId) { (didComplete) in
+				if didComplete {
+					if let dismiss = dismiss {
+						dismiss()
+					} else {
+						presentationMode.wrappedValue.dismiss()
+					}
+				} else {
+					showLoadingView = false
+				}
+			}
+		}, label: {
+			VStack(spacing: 2) {
+				Text(trialIsSelected ? "startButton" : "Continue")
+					.font(.system(size: DZMonetization.UI.shouldUppercase ? 22 : 24, weight: .bold, design: DZMonetization.UI.fontDesign))
+					.textCase(DZMonetization.UI.shouldUppercase ? .uppercase : .none)
+					.multilineTextAlignment(.center)
+					.fixedSize(horizontal: false, vertical: true)
+			}
+			.padding(.vertical, 20)
+			.frame(width: frameWidth - 50)
+			.foregroundColor(DZMonetization.UI.textButtonColor)
+			.background(DZMonetization.UI.accentGradient)
+			.cornerRadius(DZMonetization.UI.buttonRadius)
+			.shadow(color: .black.opacity(0.2), radius: 8, x: 0, y: 4)
+		})
+	}
+	
+	func simpleSuccess() {
+		let generator = UINotificationFeedbackGenerator()
+		generator.notificationOccurred(.success)
+	}
+	
+}
+
+struct AppImageView: View {
+	@State var size: CGFloat
+	
+	var body: some View {
+		Image("paywall-image")
+			.resizable()
+			.scaledToFit()
+			.frame(height: size)
+	}
 }
 
 struct GetAccessView: View {
-    @State var size: CGFloat = 32
+	@State var size: CGFloat = DZMonetization.UI.shouldUppercase ? 29 : 32
     
     var body: some View {
         VStack {
@@ -228,20 +242,12 @@ struct CheckmarkToggleStyle: ToggleStyle {
             configuration.label
             Spacer()
             Rectangle()
-                .foregroundColor(configuration.isOn ? DZMonetization.UI.accent : .white.opacity(0.62))
+                .foregroundColor(configuration.isOn ? DZMonetization.UI.accent : DZMonetization.UI.accent.opacity(0.32))
                 .frame(width: 54, height: 31, alignment: .center)
                 .overlay(
                     Circle()
                         .foregroundColor(.white)
                         .padding(.all, 3)
-                    //                        .overlay(
-                    //                            Image(systemName: configuration.isOn ? "checkmark" : "xmark")
-                    //                                .resizable()
-                    //                                .aspectRatio(contentMode: .fit)
-                    //                                .font(Font.title.weight(.black))
-                    //                                .frame(width: 8, height: 8, alignment: .center)
-                    //                                .foregroundColor(configuration.isOn ? Color.Custom.paywallAccent : .gray)
-                    //                        )
                         .offset(x: configuration.isOn ? 11 : -11, y: 0)
                         .animation(Animation.spring(response: 0.2, dampingFraction: 0.56, blendDuration: 0.12))
                     
@@ -286,7 +292,7 @@ struct RestoreButtonView: View {
             }
         }, label: {
             Text("RestorePurchase")
-                .font(.system(size: 12, weight: Font.Weight.medium, design: DZMonetization.UI.fontDesign))
+				.font(.system(size: DZMonetization.UI.shouldUppercase ? 11 : 12, weight: Font.Weight.medium, design: DZMonetization.UI.fontDesign))
 				.underline()
 				.textCase(DZMonetization.UI.shouldUppercase ? .uppercase : .none)
                 .foregroundColor(DZMonetization.UI.textColor.opacity(0.9))
@@ -306,7 +312,7 @@ struct TermsAndConditionView: View {
             isShowingTerms = true
         }, label: {
             Text("Terms & Conditions")
-                .font(.system(size: 11, weight: Font.Weight.medium, design: DZMonetization.UI.fontDesign))
+				.font(.system(size: DZMonetization.UI.shouldUppercase ? 10 : 11, weight: Font.Weight.medium, design: DZMonetization.UI.fontDesign))
 				.underline()
 				.textCase(DZMonetization.UI.shouldUppercase ? .uppercase : .none)
                 .foregroundColor(DZMonetization.UI.textColor).opacity(0.9)
@@ -325,7 +331,7 @@ struct PolicyLinkView: View {
             isShowingPolicy = true
         }, label: {
             Text("Privacy Policy")
-                .font(.system(size: 11, weight: Font.Weight.medium, design: DZMonetization.UI.fontDesign))
+				.font(.system(size: DZMonetization.UI.shouldUppercase ? 10 : 11, weight: Font.Weight.medium, design: DZMonetization.UI.fontDesign))
 				.underline()
 				.textCase(DZMonetization.UI.shouldUppercase ? .uppercase : .none)
                 .foregroundColor(DZMonetization.UI.textColor).opacity(0.9)
