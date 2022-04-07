2a51bb2740e1fe822195b8041175f8518d7242f4
Daniel Zanchi <danny.zanchi@gmail.com>
2022-04-07 21:24:30 +0200

    downgrade

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -15,7 +15,7 @@ let package = Package(
             targets: ["DZMonetization"]),
     ],
     dependencies: [
-        .package(name: "DZDataAnalytics", url: "https://github.com/DanielZanchi/DZAnalytics.git", branch: "main")
+        .package(name: "DZDataAnalytics", url: "https://github.com/DanielZanchi/DZAnalytics.git", .branchItem("main"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
