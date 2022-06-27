a7dc0ccab6c4335c86472acee24447e6c2e2a275
Daniel Zanchi <danny.zanchi@gmail.com>
2022-06-27 15:01:06 +0200

    time diff

--- Sources/DZMonetization/Utils/Calendar.Extensions.swift
+++ Sources/DZMonetization/Utils/Calendar.Extensions.swift
@@ -9,10 +9,14 @@ import Foundation
 
 extension Calendar {
     func numberOfDaysBetween(_ from: Date, and to: Date) -> Int {
+        
+        let diff = from.timeIntervalSinceReferenceDate - to.timeIntervalSinceReferenceDate
+        let diffComponents = dateComponents([.minute], from: Date(timeIntervalSinceReferenceDate: diff))
+        print("§§ number of days since last receipt update: \(diffComponents.minute)")
+        return diffComponents.minute ?? -1
         let fromDate = startOfDay(for: from)
         let toDate = startOfDay(for: to)
         let numberOfDays = dateComponents([.minute], from: fromDate, to: toDate)
-        print("§§ number of days since last receipt update: \(numberOfDays)")
         return numberOfDays.minute ?? -1
     }
 }
