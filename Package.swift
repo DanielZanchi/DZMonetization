9c78f7379819805ee7aa644657557ee575acfef9
Daniel Zanchi <danny.zanchi@gmail.com>
2023-03-19 11:19:52 +0100

    from not branch

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "git@github.com-DZAnalytics:DanielZanchi/DZAnalytics.git", from: "2.1.2"),
-		.package(url: "git@github.com-DZUtilities:DanielZanchi/DZUtilities.git", branch: "2.1.3"),
+		.package(url: "git@github.com-DZUtilities:DanielZanchi/DZUtilities.git", from: "2.1.3"),
 
     ],
     targets: [
